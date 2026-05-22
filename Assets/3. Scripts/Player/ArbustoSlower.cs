dad6d10d5ad61a0665ab397c408975f6f0185bfc
VhalroUni <avalerof@edu.tecnocampus.cat>
2026-05-22 16:41:50 +0200

    Polish y features
    Arreglos:
    Fog
    Skybox
    
    Cambios
    Zona caravanas
    
    Añadidos
    Script de arbusto
    LandMarks

---
+++ Assets/3. Scripts/Player/ArbustoSlower.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class ArbustoSlower : MonoBehaviour
+{
+    public float reduccionVelocidad = 2f;
+    public GameObject player;
+    public PlayerController playerMovement;
+
+    private float walkOriginal;
+    private float runOriginal;
+    private bool dentroDelArbusto = false;
+
+    private void Start()
+    {
+        walkOriginal = playerMovement.walkSpeed;
+        runOriginal = playerMovement.runSpeed;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Arbusto") && !dentroDelArbusto)
+        {
+            playerMovement.walkSpeed = walkOriginal - reduccionVelocidad;
+            playerMovement.runSpeed = runOriginal - reduccionVelocidad;
+
+            dentroDelArbusto = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Arbusto"))
+        {
+            playerMovement.walkSpeed = walkOriginal;
+            playerMovement.runSpeed = runOriginal;
+
+            dentroDelArbusto = false;
+        }
+    }
+}
