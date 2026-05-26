969ace74ed7e307fe417accd31fa511ac5236ccc
Jopaib8 <jopaib8@gmail.com>
2026-05-26 18:01:29 +0200

    "Elephant" animations and implementation

--- Assets/3. Scripts/Enemies/AI/EnemyStates/EnemyWatergunState.cs
+++ Assets/3. Scripts/Enemies/AI/EnemyStates/EnemyWatergunState.cs
@@ -3,15 +3,21 @@ using UnityEngine;
 public class EnemyWatergunState : IEnemyState
 {
     private WaterGun waterGun;
+    private Animator animator;
 
     public EnemyWatergunState(AIContext context)
     {
         waterGun = context.waterGun;
+        animator = context.GetComponentInChildren<Animator>();
     }
 
     public void Enter()
     {
         waterGun.StartWaterGun();
+        if (animator != null)
+        {
+            animator.SetTrigger("StartDefuse");
+        }
     }
 
     public void Exit()
