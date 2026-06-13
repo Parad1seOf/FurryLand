264b9b5d72014a1deaf73fa08fd12eea8ced247d
VhalroUni <avalerof@edu.tecnocampus.cat>
2026-06-13 13:42:41 +0200

    Particulas al romper elementos

--- Assets/3. Scripts/Enviorement/SimpleBreakableProp.cs
+++ Assets/3. Scripts/Enviorement/SimpleBreakableProp.cs
@@ -2,8 +2,22 @@ using UnityEngine;
 
 public class SimpleBreakableProp : MonoBehaviour, IDamageable
 {
+    [Header("FX")]
+    public ParticleSystem breakParticles;
+    public float particlesLifetime = 3f;
+
     public void Break()
     {
+        if (breakParticles != null)
+        {
+            ParticleSystem particles = Instantiate(
+                breakParticles,
+                transform.position,
+                transform.rotation);
+
+            Destroy(particles.gameObject, particlesLifetime);
+        }
+
         gameObject.SetActive(false);
     }
 
