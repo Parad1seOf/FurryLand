1afb11271ffcc5f7f7a6e2bee81ed2203dfa7085
gere149 <gere.delatorre@gmail.com>
2026-06-15 16:46:59 +0200

    Sensibilidad implementada

--- Assets/3. Scripts/Menus/SettingsManager.cs
+++ Assets/3. Scripts/Menus/SettingsManager.cs
@@ -3,15 +3,20 @@ using UnityEngine.UI;
 
 public class SettingsManager : MonoBehaviour
 {
+    [SerializeField] private FirstPersonLook firstPersonLook;
+
     [Header("UI references")]
     [SerializeField] private Slider volumeSlider;
     [SerializeField] private Slider FOVSlider;
     [SerializeField] private Toggle fogToggle;
+    [SerializeField] private Slider sensitivitySlider;
 
     [SerializeField] private Camera mainCamera;
 
     private const float DEFAULT_VOLUME = 0.5f;
     private const float DEFAULT_FOV = 60f;
+    private const float DEFAULT_SENSITIVITY = 120f;
+
 
     void Start()
     {
@@ -20,18 +25,23 @@ public class SettingsManager : MonoBehaviour
         float savedVolume = PlayerPrefs.GetFloat("MusicVolume", DEFAULT_VOLUME);
         float savedFOV = PlayerPrefs.GetFloat("CameraFOV", DEFAULT_FOV);
         int savedFog = PlayerPrefs.GetInt("FogEnabled", 1); // 1 = Activado, 0 = Desactivado
+        float savedSensitivity = PlayerPrefs.GetFloat("MouseSensitivity", DEFAULT_SENSITIVITY);
 
         if (volumeSlider != null) volumeSlider.value = savedVolume;
         if (FOVSlider != null) FOVSlider.value = savedFOV;
         if (fogToggle != null) fogToggle.isOn = savedFog == 1;
+        if (sensitivitySlider != null) sensitivitySlider.value = savedSensitivity;
+        if (firstPersonLook == null) firstPersonLook = FindFirstObjectByType<FirstPersonLook>();
 
         UpdateVolume(savedVolume);
         UpdateFOV(savedFOV);
         UpdateFog(savedFog == 1);
+        UpdateSensitivity(savedSensitivity);
 
         if (volumeSlider != null) volumeSlider.onValueChanged.AddListener(UpdateVolume);
         if (FOVSlider != null) FOVSlider.onValueChanged.AddListener(UpdateFOV);
         if (fogToggle != null) fogToggle.onValueChanged.AddListener(UpdateFog);
+        if (sensitivitySlider != null) sensitivitySlider.onValueChanged.AddListener(UpdateSensitivity);
     }
 
     public void UpdateVolume(float value)
@@ -52,5 +62,16 @@ public class SettingsManager : MonoBehaviour
     {
         RenderSettings.fog = value;
         PlayerPrefs.SetInt("FogEnabled", value ? 1 : 0);
+    }
+
+    public void UpdateSensitivity(float value)
+    {
+        if (firstPersonLook != null)
+        {
+            firstPersonLook.yawSpeed = value;
+            firstPersonLook.pitchSpeed = value;
+        }
+
+        PlayerPrefs.SetFloat("MouseSensitivity", value);
     }
 }
--- Assets/3. Scripts/Player/FirstPersonLook.cs
+++ Assets/3. Scripts/Player/FirstPersonLook.cs
@@ -8,12 +8,12 @@ public class FirstPersonLook : MonoBehaviour
     public Transform pitchController;
 
     [Header("Settings")]
-    public float yawSpeed   = 120f;
+    public float yawSpeed = 120f;
     public float pitchSpeed = 120f;
-    public float minPitch   = -80f;
-    public float maxPitch   =  80f;
+    public float minPitch = -80f;
+    public float maxPitch = 80f;
 
-    public float Yaw   { get; private set; }
+    public float Yaw { get; private set; }
     public float Pitch { get; private set; }
 
     private InputReader input;
@@ -21,15 +21,15 @@ public class FirstPersonLook : MonoBehaviour
     private void Awake()
     {
         input = GetComponent<InputReader>();
-        Yaw   = transform.eulerAngles.y;
+        Yaw = transform.eulerAngles.y;
         Pitch = pitchController != null ? pitchController.localEulerAngles.x : 0f;
     }
 
     private void Update()
     {
-        Yaw   += input.LookInput.x * yawSpeed   * Time.deltaTime;
+        Yaw += input.LookInput.x * yawSpeed * Time.deltaTime;
         Pitch -= input.LookInput.y * pitchSpeed * Time.deltaTime;
-        Pitch  = Mathf.Clamp(Pitch, minPitch, maxPitch);
+        Pitch = Mathf.Clamp(Pitch, minPitch, maxPitch);
 
         transform.rotation = Quaternion.Euler(0f, Yaw, 0f);
         if (pitchController != null)
